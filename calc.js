ff1fc5f7e0153e8ff5e0096d327724f715da736b
Khuhani <suleimanmigwi@gmail.com>
2025-05-05 11:05:08 +0300

    Functions for the 4 basic math operations created. I've tested them in the browser's console and they work as intended.

---
+++ calc.js
@@ -0,0 +1,28 @@
+//Math Operators//
+
+//Logic for adding numbers//
+function toAdd(num1,num2) {
+    return num1 + num2;
+};
+
+//Logic for subtracting numbers//
+function toSubtract(num1,num2) {
+    return num1 - num2;
+};
+
+//Logic for multiplying numbers//
+function toMultiply(num1,num2) {
+    return num1 * num2;
+};
+
+//Logic for dividing numbers//
+function toDivide(num1,num2) {
+    return num1/num2;
+};
+
+//3 Variables for math operation//
+let firstNum;
+
+let mathOperator;
+
+let secondNum;
