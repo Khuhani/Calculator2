45f5add52efdc1c371a310266af9fc149f2131f0
Khuhani <suleimanmigwi@gmail.com>
2025-05-09 11:59:58 +0300

    Most of the calculator works as intended now. The biggest bulwark was knowing how to store the entered numbers in the right variables and sequence. All that's left now is making the decimal fully functional, as I can't operate two decimal numbers in the current version of the program.

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@
 
 //Logic for adding numbers//
 function toAdd(num1,num2) {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 };
 
 //Logic for subtracting numbers//
@@ -20,11 +20,15 @@ function toDivide(num1,num2) {
     return num1/num2;
 };
 
-//3 Variables for math operation//
-let firstNum;
-let secondNum;
+//Logic for returning remainder//
+function toRemainder(num1,num2) {
+    return num1 % num2;
+};
 
-let mathOperator;
+//3 Variables for math operation//
+let firstNum = '';
+let mathOperator = '';
+let secondNum = '';
 
 //Operation logic//
 function operate(mathOperator,firstNum,secondNum) {
@@ -34,11 +38,16 @@ if(mathOperator == '+') {
     result = toAdd(firstNum,secondNum)
 } else if(mathOperator == '-') {
     result = toSubtract(firstNum,secondNum)
-} else if(mathOperator == '*') {
+} else if(mathOperator == 'x') {
     result = toMultiply(firstNum,secondNum)
-} else if(mathOperator == '/') {
+} else if(mathOperator == '÷') {
     result = toDivide(firstNum,secondNum)
-} else {
+} else if(mathOperator == '%') {
+    result = toRemainder(firstNum,secondNum)
+} else if(mathOperator == '÷' && secondNum == '0') {
+    result = 'MATH ERROR'
+}
+  else {
     result ='Invalid Operation'
 };
 
@@ -47,10 +56,60 @@ return result;
 
 //Logic for populating display//
 let content = document.querySelector(".operand");
-let buttons = document.querySelectorAll("#buttons");
+let numbers = document.querySelectorAll("#nums");
+let signs = document.querySelectorAll("#signs");
+let equalSign = document.querySelector(".equals");
+let allClear = document.querySelector(".clear");
 
-function populateDisplay() {buttons.forEach(btn => btn.addEventListener('click', e=>{
-    let buttonContent = e.target.innerText;
-    content.textContent = buttonContent;
-}))};
-populateDisplay();
+function populateNums() {numbers.forEach(btn => btn.addEventListener('click', e=>{
+    const buttonClicked = e.target.innerText;
+    secondNum += buttonClicked;
+    content.textContent = secondNum;
+}));
+};
+populateNums();
+
+function populateSigns() {
+    signs.forEach(btn => btn.addEventListener('click', e => {
+        const signClicked = e.target.innerText;
+        firstNum = secondNum;
+        mathOperator = signClicked;
+        secondNum = '';
+    }))
+};
+populateSigns();
+
+
+//Logic for working the calculator//
+function runCalc() {
+    let result = '';
+
+    result = operate(mathOperator,firstNum,secondNum);
+    secondNum = result;
+    mathOperator = '';
+};
+
+function equals() {
+    equalSign.addEventListener('click', ()=> {
+        runCalc();
+        content.textContent = secondNum;
+    })
+};
+equals();
+
+//Logic for clearing calc screen//
+function clearScreen() {
+    allClear.addEventListener('click', () => {
+        firstNum = '';
+        mathOperator = '';
+        secondNum = '';
+        content.textContent = 0;
+    })
+};
+clearScreen();
+/*AN IDEA: Consider changing the HTML element IDs such that all number buttons have their
+own ID, while all arithmetic operations have their own IDs. Create 2 node variables for each
+then create two functions using each node to populate the display. Create a new function for 
+running the calc. Create 2 variables for each of the 2 previous functions, then use them as 
+parameters in the operate function. Call the operate function.
+*/
